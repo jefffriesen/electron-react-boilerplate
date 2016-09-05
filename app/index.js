961fd0ee71020e56feb6e017801cff6da1164226
Jeff Friesen <momoblackblack@gmail.com>
2016-09-05 10:53:52 -0600

    setting up minimal fs example

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import fs from 'fs';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, hashHistory } from 'react-router';
@@ -6,6 +7,15 @@ import { syncHistoryWithStore } from 'react-router-redux';
 import routes from './routes';
 import configureStore from './store/configureStore';
 import './app.global.css';
+
+const remote = require('electron').remote;
+const appPath = remote.app.getAppPath();
+console.log('appPath: ', appPath);
+
+const irisPath = `/${appPath}/app/data/iris.json`;
+const irisData = JSON.parse(fs.readFileSync(irisPath, 'utf8'));
+console.log('irisData: ', irisData);
+
 
 const store = configureStore();
 const history = syncHistoryWithStore(hashHistory, store);
