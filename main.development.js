4fe692743fd7f7fe8cb63706816f1a5c26d39f2a
Jeff Friesen <momoblackblack@gmail.com>
2016-09-12 17:14:28 -0600

    opening devtools in production

--- main.development.js
+++ main.development.js
@@ -52,6 +52,7 @@ app.on('ready', async () => {
     mainWindow = null;
   });
 
+  mainWindow.openDevTools();
   if (process.env.NODE_ENV === 'development') {
     mainWindow.openDevTools();
     mainWindow.webContents.on('context-menu', (e, props) => {
