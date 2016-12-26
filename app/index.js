a8412693ee23c8d96f5b01336b3d54a8559b54c2
Jeff Friesen <momoblackblack@gmail.com>
2016-12-26 17:11:18 +0530

    testing fs with the latest changes to master

--- app/index.js
+++ app/index.js
@@ -3,12 +3,24 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
+import fs from 'fs';
 import routes from './routes';
 import configureStore from './store/configureStore';
 import './app.global.css';
 
 const store = configureStore();
 const history = syncHistoryWithStore(hashHistory, store);
+const remote = require('electron').remote;
+
+const appPath = remote.app.getAppPath();
+console.log('appPath              : ', appPath);
+console.log('process.resourcesPath: ', process.resourcesPath);
+
+
+const irisPath = `${appPath}/data/iris.json`;
+
+const irisData = JSON.parse(fs.readFileSync(irisPath, 'utf8'));
+console.log('irisData: ', irisData);
 
 render(
   <Provider store={store}>
